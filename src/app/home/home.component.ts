28d84d55b2301e5ebf6a4488b23a98337192851d
Bertrand2808 <bertrand2808@icloud.com>
2023-06-28 11:10:44 +0200

    add button home in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,18 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
+  constructor(private router: Router) { }
+  goToPokedex() {
+    this.router.navigate(['/pokedex']);
+  }
 
+  goToSearch() {
+    this.router.navigate(['/search']);
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 
 
 }
